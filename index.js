175b4a71bfec4f5a92617135cd243e2678645876
Seth David Gyimah <esec26@gmail.com>
2022-06-18 16:20:57 +0000

    moving items based on event listener

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-const dodger = document.getElementById("dodger");
+//grabbing dodger element / saving to variable
+const item = document.getElementById('dodger')
 
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+//function to move dodger left.
+function moveDodgerLeft(){
+  console.log(item.style.left)
+  const currentLeft = item.style.left.replace('px', '')
+  const left = parseInt(currentLeft, 10) 
+
+  //condition check if item is not at end of left field
+  if (left > 0){
+    item.style.left = `${left - 1}px`
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
+  console.log(item.style.left)
+}
 
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
-    }
+//event listener for when left arrow is pressed
+item.addEventListener('keydown', function(){
+  if (keydown === "ArrowLeft"){
+    moveDodgerLeft
   }
+})            
 
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
+
+//function to move dodger right.
+function moveDodgerRight(){
+  const currentLeft = item.style.left.replace('px', '')
+  const left = parseInt(currentLeft, 10) 
+
+
+  //condition check if item is not at end of right field
+  if (left > 0){
+    item.style.left = `${left + 1}px`
+  }
+}
+
+
+//event listener for when right arrow is pressed
+item.addEventListener('keydown', function(){
+  if (keydown === "ArrowLeft"){
+    moveDodgerRight
+  }
+})
